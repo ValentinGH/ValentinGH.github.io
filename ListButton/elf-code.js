292e0c36e725db44ffc894eaae6dd10c4b3e4386
VAL MONTGOMERY <valmontgomery@VALs-MacBook-Pro.local>
2018-10-13 16:14:08 -0700

    adding listButton assignment

--- ListButton/elf-code.js
+++ ListButton/elf-code.js
@@ -1,6 +1,6 @@
 const elfCode = {
     appendToList: (list, value) => {
-        const li = document.createElement("li");
+        const li = document.createElement('li');
         li.appendChild(document.createTextNode(value));
         list.appendChild(li);
     }
--- ListButton/index.js
+++ ListButton/index.js
@@ -1,9 +1,16 @@
 window.onload = () => {
-    const userInput = document.getElementById("list-data");
-    const setTextButton = document.getElementById("set-text");
-    setTextButton.onclick = () => {
-        userInput.value = "The first item for my list";
-
-    }
+    // PART 1
+     const userInput = document.getElementById('list-data');
+     const setTextButton = document.getElementById('set-text');
+     const listButton = document.getElementById('add-to-list');
     
+    // PART 2
+    setTextButton.onclick = () => {
+        userInput.value = '';
+    }
+    //PART 3
+    listButton.onclick = () =>
+    { const myList = document.getElementById('my-list');
+    elfCode.appendToList(myList, userInput.value);
+ }
 }
