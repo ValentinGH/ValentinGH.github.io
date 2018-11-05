0426245c98a134cf0a01aa956f15e8a41b506c7a
valentinGH <valentin.montgomery@bellevuecollege.edu>
2018-11-05 12:17:39 -0800

    adding function test

---
+++ javascript-test/function-basics.js
@@ -0,0 +1,19 @@
+function simpleFunction ()
+{
+console.log(" this is a simple function");
+}
+
+function add (a,b)
+{console.log(a +b);}
+
+function adder() {
+    console.log(2 + 3 );
+}
+
+function functionParameters(value){
+    console.log(value)
+}
+
+
+simpleFunction();
+functionParameters("many functions take parameters")
