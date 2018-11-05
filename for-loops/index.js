70936bcff30183e376bdccb334b9e4a61d6a16be
valentinGH <valentin.montgomery@bellevuecollege.edu>
2018-11-05 11:55:28 -0800

    adding function basics

--- for-loops/index.js
+++ for-loops/index.js
@@ -1,23 +1,21 @@
 window.onload = () => {
-    // PART 1
-    const userInput = document.getElementById('list-data');
-    const setTextButton = document.getElementById('set-text');
+    
+    
      const listButton = document.getElementById('run-for-loop');
+
     
     
-    setTextButton.onclick = () => {
-        userInput.value = '';
-        let i =0;
         
-    }
+    
     //PART 3
     listButton.onclick = () =>
     {
         const myList = document.getElementById('my-list');
-        for(i;i<6;i++)
-        {
+        for(i=0;i<6;i++)
+        
          
-     elfCode.appendToList(myList, i);
-    }
+    { elfCode.appendToList(myList, i);}
+    
  }
+
 }
