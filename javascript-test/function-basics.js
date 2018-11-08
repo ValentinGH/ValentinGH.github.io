3639037bb0ae0b5384bb8db91b9a513793717b65
valentinGH <valentin.montgomery@bellevuecollege.edu>
2018-11-07 22:17:14 -0800

    adding function basics

--- javascript-test/function-basics.js
+++ javascript-test/function-basics.js
@@ -1,19 +1,49 @@
-function simpleFunction ()
+window.onload =() => {
+    const FunctionCalls= document.getElementById("functionCalls"); 
+    const callUserInput = document.getElementById("callUserInput");
+    
+    FunctionCalls.onclick = () => {
+        simpleFunction();
+        functionParameters("many functions take parameters");
+        functionReturn();
+        const returnValue = functionReturn();
+        
+        console.log(returnValue);
+    }
+  callUserInput.onclick=() => {
+        const userInputParagraph = document.getElementById("showUserInput");
+        const UserInput = document.getElementById("userInput");
+        userInputParagraph.textContent = UserInput.value;
+
+    
+        
+    }
+
+}
+
+    function simpleFunction()
+    {
+        const displayText = "function should consist of statements designed to perform a single task";
+        const simpleFunctionParagraph = document.getElementById("simpleFunction");
+        console.log(displayText);
+        simpleFunctionParagraph.textContent = displayText;
+    }
+
+    function functionParameters(value)
+        {
+
+            const simpleFunctionParagraph = document.getElementById("functionParameters");
+            console.log(value);
+            simpleFunctionParagraph.textContent = value;
+
+        }
+    
+function functionReturn()
 {
-console.log(" this is a simple function");
+    const returnText = "Many functions return values";
+    const simpleReturnParagraph = document.getElementById("functionReturn");
+    console.log(returnText);
+    simpleReturnParagraph.textContent = returnText;
 }
 
-function add (a,b)
-{console.log(a +b);}
-
-function adder() {
-    console.log(2 + 3 );
-}
-
-function functionParameters(value){
-    console.log(value)
-}
-
-
-simpleFunction();
-functionParameters("many functions take parameters")
+    
